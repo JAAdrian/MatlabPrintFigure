2212bdc2fbb25f1282e3c62c1f93689f0dee268e
Jens-Alrik Adrian <jens-alrik.adrian@jade-hs.de>
2017-02-27 22:09:09 +0100

    Change affiliation and license

--- @PrintFigure/PrintFigure.m
+++ @PrintFigure/PrintFigure.m
@@ -1,18 +1,18 @@
 classdef PrintFigure < handle & matlab.System
 %PrintFigure A Class for Easy and Reproducible Figure Formatting And Printing in MATLAB
 % -------------------------------------------------------------------------
-% 
+%
 % PrintFigure Properties:
 %	propA - <description>
 %	propB - <description>
-% 
+%
 % PrintFigure Methods:
 %	doThis - <description>
 %	doThat - <description>
-% 
-% Author :  J.-A. Adrian (JA) <jens-alrik.adrian AT jade-hs.de>
+%
+% Author :  J.-A. Adrian (JA) <jensalrik.adrian AT gmail.com>
 % Date   :  26-Jun-2015 17:54:58
-% 
+%
 
 
 properties (Constant, Hidden)
@@ -239,39 +239,5 @@ end
 
 
 
-
-
-
-%-------------------- Licence ---------------------------------------------
-% Copyright (c) 2015, J.-A. Adrian
-% Institute for Hearing Technology and Audiology
-% Jade University of Applied Sciences
-% All rights reserved.
-% 
-% Redistribution and use in source and binary forms, with or without
-% modification, are permitted provided that the following conditions are
-% met:
-%     * Redistributions of source code must retain the above copyright
-%       notice, this list of conditions and the following disclaimer.
-%     * Redistributions in binary form must reproduce the above copyright
-%       notice, this list of conditions and the following disclaimer in the
-%       documentation and/or other materials provided with the
-%       distribution.
-%     * Neither the name of the <organization> nor the
-%       names of its contributors may be used to endorse or promote
-%       products derived from this software without specific prior written
-%       permission.
-% 
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-% IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-% THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-% PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE
-% FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-% CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-% SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-% INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
-% THE POSSIBILITY OF SUCH DAMAGE.
 
 % End of file: PrintFigure.m
--- @PrintFigure/private/viridis.m
+++ @PrintFigure/private/viridis.m
@@ -10,7 +10,7 @@ function [map] = viridis(n)
 %  Output:   ---------
 %
 %
-% Author :  J.-A. Adrian (JA) <jens-alrik.adrian AT jade-hs.de>
+% Author :  J.-A. Adrian (JA) <jensalrik.adrian AT gmail.com>
 % Date   :  14-Jul-2015 11:37:48
 % Updated:  <>
 %
@@ -282,41 +282,4 @@ map = [...
 P   = size(map,1);
 map = interp1(1:size(map,1), map, linspace(1,P,n), 'linear');
 
-
-
-
-
-
-%-------------------- Licence ---------------------------------------------
-% Copyright (c) 2015, J.-A. Adrian
-% Institute for Hearing Technology and Audiology
-% Jade University of Applied Sciences
-% All rights reserved.
-%
-% Redistribution and use in source and binary forms, with or without
-% modification, are permitted provided that the following conditions are
-% met:
-%
-%	1. Redistributions of source code must retain the above copyright
-%	   notice, this list of conditions and the following disclaimer.
-%
-%	2. Redistributions in binary form must reproduce the above copyright
-%	   notice, this list of conditions and the following disclaimer in
-%	   the documentation and/or other materials provided with the
-%	   distribution.
-%
-%	3. Neither the name of the copyright holder nor the names of its
-%	   contributors may be used to endorse or promote products derived
-%	   from this software without specific prior written permission.
-%
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-% IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-% TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-% HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-% SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+% End of file: viridis.m
